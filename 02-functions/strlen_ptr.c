bf7de514f2fa751200467988d607ae41423f7c05
Ivy <garfieldivymaitland@gmail.com>
2025-12-19 08:54:34 -0500

    Implement the helper function and part of the main function

--- 02-functions/strlen_ptr.c
+++ 02-functions/strlen_ptr.c
@@ -6,9 +6,25 @@
  */
 
 #include <stdio.h>
-#include <stddef.h> 
+#include <stddef.h> // for size_t type
 
-int main(){
-    
+/* It is important to declare the fuction prior to calling the
+    function. Else the compiler will not be able to compile the
+    program efficiently or at all.
+*/
+size_t my_strlen(const char *str) {
+    const char *s = str;
+
+    while (*s) {
+        s++;
+    }
+
+    return s - str;
+}
+
+int main() {
+    // 40 character sha-1 hexanumeric git commit hash
+    char str[] = "bc4dfse3a7f9e2d1c8b6a5e4f3d2c1b0a9e8d7c6" 
+
 }
 
